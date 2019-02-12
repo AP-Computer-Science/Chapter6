d99d37fad56905cc80ebb2849aa03fe55230db3f
Brian Barnes <brian.jx.b@gmail.com>
2019-02-12 14:35:11 -0600

    Finished bank stuff.

--- src/Account.java
+++ src/Account.java
@@ -3,6 +3,7 @@ public class Account {
     private final double RATE = 0.035; // interest rate of 3.5%
     private int acctNumber;
     private double balance;
+    private int invokeNum = 0;
     private String name;
     public Account(String owner, int account, double initial) {
         name = owner;
@@ -21,10 +22,24 @@ public class Account {
         return balance;
     }
     public void deposit(double amount) {
+        if(invokeNum == 100) {
+            balance += balance * 0.3;
+            invokeNum = 0;
+            balance += amount;
+            return;
+        }
         balance += amount;
+        invokeNum++;
     }
     public void withdraw(double amount) {
+        if(invokeNum == 100) {
+            balance += balance * 0.3;
+            invokeNum = 0;
+            balance -= amount;
+            return;
+        }
         balance -= amount;
+        invokeNum++;
     }
     public void move(Account destination, double amount) {
         if(amount > balance) {
--- src/BankArray.java
+++ src/BankArray.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BankArray {
@@ -7,16 +9,54 @@ public class BankArray {
         //Create account
         int count = 0;
         while(count < accounts.length) {
-            System.out.print("Open account [Y/N]? ");
+            System.out.print("Open account " + (count + 1) + " [Y/N]? ");
             char input = s.next()
                           .trim()
                           .toUpperCase()
                           .charAt(0);
             if(input == 'Y') {
                 System.out.print("Enter account holder name: ");
+                s = new Scanner(System.in);
                 String accountHolder = s.nextLine();
-                Account a = new Account(accountHolder, (int)(Math.random() * 99999999));
+                Account a = new Account(accountHolder, (int)(Math.random() * 99999999), (int)(Math.random() * 1000000001));
+                accounts[count] = a;
                 count++;
+            }
+            else {
+               break; 
+            }
+        }
+        while(true) {
+            s = new Scanner(System.in);
+            System.out.println("Accounts");
+            System.out.println("========");
+            for(int i = 0; i < count; i++) {
+                System.out.println(i + ": " + accounts[i].getName());
+            }
+            System.out.print("Enter account number to manage: ");
+            int num = Math.abs(s.nextInt());
+            if(num >= count) {
+                System.out.println("No such account exist.");
+            }
+            else {
+                Account instance = accounts[num];
+                System.out.println("Account has " + java.text.DecimalFormat.getCurrencyInstance().format(instance.getBalance()));
+                System.out.print("Transfer money [Y/N]? ");
+                char input = s.next()
+                          .trim()
+                          .toUpperCase()
+                          .charAt(0);
+                if(input == 'Y') {
+                    System.out.print("Enter number of account to transfer money to: ");
+                    int transferAccount = Math.abs(s.nextInt());
+                    if(transferAccount >= count) {
+                        System.out.println("No such account exist.");
+                        continue;
+                    }
+                    System.out.print("Enter amount to transfer: ");
+                    int amount = Math.abs(s.nextInt());
+                    instance.move(accounts[transferAccount], amount);
+                }
             }
         }
     }
